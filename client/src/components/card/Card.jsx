7b3d811325472aac0fc1a3ad8b993c88b3e00106
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-25 11:31:22 +0100

    Modular Card component

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+// custom tools
+import IconFav from "../icon/IconFavorite";
+// styles
+import "./../../styles/icon-color.css";
 
-export default function Card() {
+export default function Card({resourceType, data}) {
+
     return (
-        <div>
-            TODO@gui : generic component using children prop
-        </div>
+      <>
+      <Link to={`/${resourceType}/${data._id}`} className="link">
+      <h3 className="title">{data.title}</h3>
+      <img src={data.cover} alt={data.title} className="cover"/>
+      </Link>
+      <IconFav resourceType={resourceType} resourceId={data._id} isAlreadyFavorite={data.isFavorite} />
+      </>
     )
 }
--- client/src/components/card/CardAlbum.jsx
+++ client/src/components/card/CardAlbum.jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// custom tools
-import IconFav from "../icon/IconFavorite";
-// styles
-import "./../../styles/icon-color.css";
+import Card from './Card'
 
 export default function CardArtist({ data }) {
-  return (
-    <>
-    <Link to={`/albums/${data._id}`} className="link">
-    <h3 className="title">{data.title}</h3>
-    <img src={data.cover} alt={data.title} className="cover"/>
-    </Link>
-    <IconFav resourceType="albums" resourceId={data._id} isAlreadyFavorite={data.isFavorite} />
-    </>
-  )
+  return <Card resourceType='albums' data = {data} />
 }
